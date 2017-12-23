e2cf1827011fbb210728d9563725fecf4d8c097e
xezon <pascalkross@gmail.com>
2017-12-23 11:50:01 +0100

    Add project files

---
+++ src/zevents.cpp
@@ -0,0 +1,82 @@
+
+#define ZORRO_DLLMAIN
+#define ZORRO_EXPECT_ALL_FUNCTIONS
+#define ZORRO_USE_EVENT_CLASS
+#include <zorro_impl.h>
+#include <common/types.h>
+
+void CZorroEvents::main()
+{
+	set(EZorroFlag::OPENEND);
+	set(EZorroFlag::TICKS);
+	LookBack = 0;
+	BarPeriod = PERIOD_D1;
+	StartDate = 20130101;
+	EndDate = 20150101;
+
+	for (uint32_t assetId = 0; string assetName = Assets[assetId]; ++assetId)
+	{
+		asset(assetName);
+	}
+}
+
+void CZorroEvents::run()
+{
+
+}
+
+void CZorroEvents::tick()
+{
+
+}
+
+void CZorroEvents::tock()
+{
+
+}
+
+void CZorroEvents::click(int row, int col)
+{
+	(void)row;
+	(void)col;
+}
+
+void CZorroEvents::evaluate(const PERFORMANCE* pPerformance)
+{
+	(void)pPerformance;
+}
+
+var CZorroEvents::objective()
+{
+	return objectivePRR();
+}
+
+EOrderResult CZorroEvents::order(EOrderAction type)
+{
+	(void)type;
+
+	return EOrderResult::OK;
+}
+
+var CZorroEvents::neural(ENeuralMode mode, int model, int numSignals, const void* pData)
+{
+	(void)mode;
+	(void)model;
+	(void)numSignals;
+	(void)pData;
+
+	return 0.0;
+}
+
+EBarAction CZorroEvents::bar(cvars open, cvars high, cvars low, cvars close, cvars price, DATE start, DATE time)
+{
+	(void)open;
+	(void)high;
+	(void)low;
+	(void)close;
+	(void)price;
+	(void)start;
+	(void)time;
+
+	return EBarAction::CLOSE_NORMALLY;
+}
